49ccff8d5c07b26b7ba06295c30ee59d950c048a
Darren Temple <darren.temple@theodi.org>
2024-01-04 16:52:32 +0000

    - Initial commit - A fully working version ready for import or Flask use, including some commented code for reference of exact needs now just handled by 'try/except'

---
+++ app.py
@@ -0,0 +1,230 @@
+import json
+import requests
+from bs4 import BeautifulSoup
+from flask import Flask, request
+from inspect import stack
+from termcolor import colored
+
+# ----------------------------------------------------------------------------------------------------
+
+application = Flask(__name__)
+
+# ----------------------------------------------------------------------------------------------------
+
+def set_message(message, messageType=None):
+    if (messageType == 'warning'):
+        print(colored('WARNING: ' + message, 'yellow'))
+    elif (messageType == 'error'):
+        print(colored('ERROR: ' + message, 'red'))
+    else:
+        print(message)
+
+# ----------------------------------------------------------------------------------------------------
+
+def try_requests(url, numTriesMax=10):
+    r = None
+    numTries = 0
+
+    while (True):
+        if (numTries == numTriesMax):
+            message = 'Max. tries (%i) reached for: %s' % (numTriesMax, url)
+            set_message(message, 'warning')
+            break
+        r = requests.get(url)
+        numTries += 1
+        if (r.status_code == 200):
+            break
+
+    return r, numTries
+
+# ----------------------------------------------------------------------------------------------------
+
+@application.route('/catalogue-urls')
+def get_catalogue_urls():
+    catalogueUrls = []
+
+    collectionUrl = 'https://openactive.io/data-catalogs/data-catalog-collection.jsonld'
+    collectionPage, numTries = try_requests(collectionUrl)
+
+    # if not (type(collectionPage) == requests.models.Response
+    #     and collectionPage.status_code == 200
+    #     and 'json' in dir(collectionPage)
+    #     and type(collectionPage.json()) == dict
+    #     and 'hasPart' in collectionPage.json().keys()
+    #     and type(collectionPage.json()['hasPart']) == list
+    #     and all([type(i)==str for i in collectionPage.json()['hasPart']])
+    # ):
+    #     message = 'Can\'t get collection at: %s' % collectionUrl
+    #     set_message(message, 'error')
+    # else:
+    #     catalogueUrls.extend(collectionPage.json()['hasPart'])
+
+    try:
+        if (all([type(i)==str for i in collectionPage.json()['hasPart']])):
+            catalogueUrls.extend(collectionPage.json()['hasPart'])
+        else:
+            raise Exception()
+    except:
+        message = 'Can\'t get collection at: %s' % collectionUrl
+        set_message(message, 'error')
+
+    return catalogueUrls
+
+# ----------------------------------------------------------------------------------------------------
+
+@application.route('/dataset-urls')
+def get_dataset_urls():
+    datasetUrls = []
+
+    catalogueUrls = get_catalogue_urls()
+
+    for catalogueUrl in catalogueUrls:
+        cataloguePage, numTries = try_requests(catalogueUrl)
+
+        # if not (type(cataloguePage) == requests.models.Response
+        #     and cataloguePage.status_code == 200
+        #     and 'json' in dir(cataloguePage)
+        #     and type(cataloguePage.json()) == dict
+        #     and 'dataset' in cataloguePage.json().keys()
+        #     and type(cataloguePage.json()['dataset']) == list
+        #     and all([type(i)==str for i in cataloguePage.json()['dataset']])
+        # ):
+        #     message = 'Can\'t get catalogue at: %s' % catalogueUrl
+        #     set_message(message, 'error')
+        # else:
+        #     datasetUrls.extend(cataloguePage.json()['dataset'])
+
+        try:
+            if (all([type(i)==str for i in cataloguePage.json()['dataset']])):
+                datasetUrls.extend(cataloguePage.json()['dataset'])
+            else:
+                raise Exception()
+        except:
+            message = 'Can\'t get catalogue at: %s' % catalogueUrl
+            set_message(message, 'error')
+
+    return datasetUrls
+
+# ----------------------------------------------------------------------------------------------------
+
+@application.route('/feeds')
+def get_feeds():
+    feeds = []
+
+    datasetUrls = get_dataset_urls()
+
+    for datasetUrl in datasetUrls:
+        datasetPage, numTries = try_requests(datasetUrl)
+
+        try:
+            if (datasetPage.status_code == 200):
+                soup = BeautifulSoup(datasetPage.text, 'html.parser')
+            else:
+                raise Exception()
+            for script in soup.head.find_all('script'):
+                if (    'type' in script.attrs.keys()
+                    and script['type'] == 'application/ld+json'
+                ):
+                    jsonld = json.loads(script.string)
+                    if ('distribution' in jsonld.keys()):
+                        for feedIn in jsonld['distribution']:
+                            feedOut = {}
+
+                            try:
+                                feedOut['name'] = jsonld['name']
+                            except:
+                                feedOut['name'] = ''
+                            try:
+                                feedOut['type'] = feedIn['name']
+                            except:
+                                feedOut['type'] = ''
+                            try:
+                                feedOut['url'] = feedIn['contentUrl']
+                            except:
+                                feedOut['url'] = ''
+                            try:
+                                feedOut['datasetUrl'] = datasetUrl
+                            except:
+                                feedOut['datasetUrl'] = ''
+                            try:
+                                feedOut['discussionUrl'] = jsonld['discussionUrl']
+                            except:
+                                feedOut['discussionUrl'] = ''
+                            try:
+                                feedOut['licenseUrl'] = jsonld['license']
+                            except:
+                                feedOut['licenseUrl'] = ''
+                            try:
+                                feedOut['publisherName'] = jsonld['publisher']['name']
+                            except:
+                                feedOut['publisherName'] = ''
+
+                            if (len(feedOut.keys()) > 1):
+                                feeds.append(feedOut)
+        except:
+            message = 'Can\'t get dataset at: %s' % datasetUrl
+            set_message(message, 'error')
+
+    return feeds
+
+# ----------------------------------------------------------------------------------------------------
+
+@application.route('/opportunities')
+def get_opportunities(feedUrl=None):
+    opportunities = {}
+
+    if (stack()[1].function == 'dispatch_request'):
+        feedUrl = request.args.get('url')
+
+    if (not feedUrl):
+        if (stack()[1].function == 'dispatch_request'):
+            message = 'Feed URL must be given as a parameter via "/opportunities?url=your-feed-url"'
+            set_message(message, 'warning')
+            return message
+        else:
+            message = 'Feed URL must be given as a parameter via "get_opportunities(\'your-feed-url\')"'
+            set_message(message, 'warning')
+            return
+
+    set_opportunities(feedUrl, opportunities)
+
+    return list(opportunities.values())
+
+# ----------------------------------------------------------------------------------------------------
+
+def set_opportunities(feedUrl, opportunities):
+    feedPage, numTries = try_requests(feedUrl)
+
+    # if not (type(feedPage) == requests.models.Response
+    #     and feedPage.status_code == 200
+    #     and 'json' in dir(feedPage)
+    #     and type(feedPage.json()) == dict
+    # ):
+    #     message = 'Can\'t get feed at: %s' % feedUrl
+    #     set_message(message, 'error')
+
+    try:
+        for item in feedPage.json()['items']:
+            if (    'id' in item.keys()
+                and 'state' in item.keys()
+                and 'modified' in item.keys()
+            ):
+                if (item['state'] == 'updated'):
+                    if (    item['id'] not in opportunities.keys()
+                        or  item['modified'] > opportunities[item['id']]['modified']
+                    ):
+                        opportunities[item['id']] = item
+                elif (  item['state'] == 'deleted'
+                    and item['id'] in opportunities.keys()
+                ):
+                    del(opportunities[item['id']])
+        if (feedPage.json()['next'] != feedUrl):
+            set_opportunities(feedPage.json()['next'], opportunities)
+    except:
+        message = 'Can\'t get feed at: %s' % feedUrl
+        set_message(message, 'error')
+
+# ----------------------------------------------------------------------------------------------------
+
+if (__name__ == '__main__'):
+    application.run()
