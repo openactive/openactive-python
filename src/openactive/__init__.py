4fb358023ba4f3eeba36326d52b3d146e7c1ff33
Darren Temple <darren.temple@theodi.org>
2026-01-04 21:44:52 +0000

    - Updated __init__.py based on function changes and additions in the main code

--- src/openactive/__init__.py
+++ src/openactive/__init__.py
@@ -10,8 +10,9 @@ from .openactive import (
     get_partner_feed_url,
     get_opportunities,
     get_item_kinds,
-    get_item_data_types,
+    get_item_types,
     get_event_type,
+    get_superevent_id_v_subevent_ids,
     get_superevents,
     get_subevents,
 )
